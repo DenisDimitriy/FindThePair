b004d59ed54d29c23ac5b00370da027f3c755e30
Denis <denis.dimitriy@gmail.com>
2017-11-11 12:05:53 +0200

    add files: -style.css -img number
    change:
    -game.js:
    --add onclick handler
    -game.html:
    --add board with cards

--- js/game.js
+++ js/game.js
@@ -1,4 +1,28 @@
 /**
  * Created by Denis on 08.11.2017.
  */
-alert (getRandomPairArray(10));
+//Глобальные переменные
+var countSelected, selected;
+
+//создать массив случайных пар чисел
+var RandomPairArray = getRandomPairArray(6);
+
+//запомнить набор блоков card в коллекцию
+var cards = document.getElementsByClassName("card");
+
+//запомнить блок board
+var board = document.getElementsByClassName("board")[0];
+
+//Инициализировать блоки карт согласно массиву случайных пар чисел
+for (var i = 0; i < RandomPairArray.length; i++) {
+    cards[i].dataset.number = RandomPairArray[i];
+    var url = "url(img/numbers/" + cards[i].dataset.number + ".png)";
+    cards[i].style.backgroundImage = url;
+}
+
+board.onclick = function (event) {
+    var target = event.target;
+    if (target.classList.contains("card")){
+        alert (target.dataset.number);
+    }
+};
