feb12cf6fc2bbe924f1a76f05ed6a41ab7ec4369
Denis <denis.dimitriy@gmail.com>
2017-11-12 23:15:25 +0200

    add skin a working prototype is complete

--- js/game.js
+++ js/game.js
@@ -2,10 +2,16 @@
  * Created by Denis on 08.11.2017.
  */
 //Глобальные переменные
-var countSelected = 0, selected = null;
+var countOfPair = 6,
+    theme = "numbers",
+    skin = "hearthstone",
+    selected = null,
+    counter = 0,
+    blockSelect = false;
+
 
 //создать массив случайных пар чисел
-var RandomPairArray = getRandomPairArray(6);
+var RandomPairArray = getRandomPairArray(countOfPair);
 
 //запомнить набор блоков card в коллекцию
 var cards = document.getElementsByClassName("card");
@@ -16,31 +22,63 @@ var board = document.getElementsByClassName("board")[0];
 //Инициализировать блоки карт согласно массиву случайных пар чисел
 for (var i = 0; i < RandomPairArray.length; i++) {
     cards[i].dataset.number = RandomPairArray[i];
-    var url = "url(img/numbers/" + cards[i].dataset.number + ".png)";
+    var url = "url(img/themes/numbers/" + cards[i].dataset.number + ".png)";
     cards[i].style.backgroundImage = url;
+    cards[i].classList.add("open");
 }
 
+
+//Отвернуть все карты
+var timerStart = setTimeout(function () {
+    for (i = 0; i < cards.length; i++) {
+        turnCard(cards[i]);
+    }
+}, 2000);
+
+//Обработчики событий
 //Клик в пределах доски
 board.onclick = function (event) {
     var target = event.target;
 
     //клик на карту
-    if (target.classList.contains("card")) {
+    if (target.classList.contains("card") && blockSelect != true) {
+
+        //если ниодна карта еще не выбрана
         if (!selected) {
             selected = target;
-            console.log(selected);
+            openCard(selected);
         }
+
+        //если уже выбрана одна карта
         else if (selected) {
-            if (selected.dataset.number == target.dataset.number && selected!=target) {
-                alert ("The pair is found!");
-                target.classList.remove("card");
-                target.classList.add("spacer");
-                target.style.backgroundImage = "none";
-                selected.classList.remove("card");
-                selected.classList.add("spacer");
-                selected.style.backgroundImage = "none";
+
+            //если номера целевой и выбранной карт не совпадают и это не одна и та же карта
+            if (selected.dataset.number != target.dataset.number && selected != target) {
+                blockSelect = true;
+                openCard(target);
+                timerShow = setTimeout(function () {
+                    closeCard(selected);
+                    closeCard(target);
+                    selected = null;
+                    blockSelect = false;
+                }, 1000)
             }
-            selected = null;
+
+            //если номера целевой и выбранной карт совпадают и это не одна и та же карта
+            if (selected.dataset.number == target.dataset.number && selected != target) {
+                blockSelect = true;
+                openCard(target);
+                timerShow = setTimeout(function () {
+                    removeCard(selected);
+                    removeCard(target);
+                    selected = null;
+                    blockSelect = false;
+                    counter++;
+                    if (counter == countOfPair) {
+                        finishGame();
+                    }
+                }, 1000);
+            }
         }
     }
     return false;
--- js/model.js
+++ js/model.js
@@ -18,5 +18,32 @@ function getRandomPairArray(dimension) {
         index = Math.floor(Math.random() * arraySecond.length);
         arrayRezult[i] = arraySecond.splice(index, 1);
     }
-    return (arrayRezult);
+    return arrayRezult;
+}
+
+function removeCard(element) {
+    element.classList.remove("card");
+    element.classList.add("spacer");
+    element.style.backgroundImage = "none";
+}
+
+function openCard(element) {
+    element.style.backgroundImage = "url(img/themes/" + theme + "/" + element.dataset.number + ".png)";
+}
+
+function closeCard(element) {
+    element.style.backgroundImage = "url(img/skins/" + skin + ".png)";
+}
+
+function turnCard(element) {
+    if (element.classList.contains("open")) {
+        closeCard(element);
+    }
+    else {
+        openCard(element);
+    }
+}
+
+function finishGame() {
+    alert("Finish!!!");
 }
