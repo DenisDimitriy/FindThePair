cc3872de3ec1579e4cd9d9b0146d7ffef472ffb6
Denis <denis.dimitriy@gmail.com>
2017-11-19 15:47:23 +0200

    add animation of card removing

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
  * Created by Denis on 08.11.2017.
  */
 //Глобальные переменные
-var countOfPair = 6,
+var countOfPair = 12,
     theme = "numbers",
     skin = "hearthstone",
     selected = null,
@@ -12,25 +12,9 @@ var countOfPair = 6,
 //создать массив случайных пар чисел
 var RandomPairArray = getRandomPairArray(countOfPair);
 
-//запомнить блок board
-var board = document.getElementsByClassName("board")[0];
+var board = initBoard(countOfPair);
 
-var boardWidth;
-switch (countOfPair) {
-    case 6:
-        boardWidth = '448px';
-        break;
-    case 8:
-        boardWidth = '448px';
-        break;
-    case 10:
-        boardWidth = '560px';
-        break;
-    case 12:
-        boardWidth = '672px';
-        break;
-}
-board.style.width = boardWidth;
+document.querySelector(".discard-stack").style.width = parseInt(getComputedStyle(board).width, 10) + "px";
 
 //запомнить набор блоков card в коллекцию
 var cards = document.getElementsByClassName("card");
@@ -40,7 +24,6 @@ for (var i = 0; i < RandomPairArray.length; i++) {
     cards[i].dataset.number = RandomPairArray[i];
     var front = cards[i].querySelector(".front");
     front.style.backgroundImage = 'url("img/skins/' + skin + '.png")';
-    console.log (front);
     var back = cards[i].querySelector(".back");
     back.style.backgroundImage = 'url("img/themes/' + theme + '/' + cards[i].dataset.number + '.png")';
 }
@@ -53,7 +36,7 @@ for (i = 0; i < cards.length; i++) {
 setTimeout(function () {
     for (i = 0; i < cards.length; i++) {
         closeCard(cards[i]);
-    };
+    }
     disableSelect = false;
 }, 2000);
 
@@ -92,24 +75,27 @@ board.onclick = function (event) {
             //если номера целевой и выбранной карт совпадают и это не одна и та же карта
             if (selected.dataset.number == target.dataset.number && selected != target) {
                 //disableSelect = true;
-                openCard(target);
-                var card1 = selected;
-                var card2 = target;
+                //openCard(target);
+                var card1 = removeCard(selected);
+                var card2 = removeCard(target);
                 selected = null;
                 target = null;
                 counter++;
                 if (counter == countOfPair) {
                     finishGame();
                 }
-                setTimeout(function () {
-                    removeCard(card1);
-                    removeCard(card2);
-                }, 500);
+                /*
+                 setTimeout(function () {
+                 removeCard(card1);
+                 removeCard(card2);
+                 }, 500);
+                 */
             }
         }
     }
     return false;
 };
+
 
 function finishGame() {
     document.querySelector("#message").innerHTML = "FINISH";
--- js/model.js
+++ js/model.js
@@ -21,14 +21,101 @@ function getRandomPairArray(dimension) {
     return arrayRezult;
 }
 
+function initBoard(countOfPair) {
+    var board = document.querySelector(".board");
+
+    var boardWidth;
+    switch (countOfPair) {
+        case 6:
+            boardWidth = '448px';
+            break;
+        case 8:
+            boardWidth = '448px';
+            break;
+        case 10:
+            boardWidth = '560px';
+            break;
+        case 12:
+            boardWidth = '672px';
+            break;
+    }
+    board.style.width = boardWidth;
+
+    for (var i = 0; i < countOfPair * 2; i++) {
+        var newCard = document.createElement("div");
+        var newFlipper = document.createElement("div");
+        var newFront = document.createElement("div");
+        var newBack = document.createElement("div");
+        newCard.className = "card flip-container";
+        newFlipper.className = "flipper";
+        newFront.className = "front";
+        newBack.className = "back";
+        newFlipper.appendChild(newFront);
+        newFlipper.appendChild(newBack);
+        newCard.appendChild(newFlipper);
+        board.appendChild(newCard);
+    }
+
+
+    return board;
+}
+
 function removeCard(element) {
+
+    //Инициализировать контейнеры
+    var gameSpace = document.body.querySelector('.game-space');
+    var discardStack = document.querySelector(".discard-stack");
+
+    //Создать клоны
+    var newElement1 = element.cloneNode(true);
+    var newElement2 = element.cloneNode(true);
+
+    //Удалить исходную карту
     element.innerHTML = null;
     element.className = "spacer";
-    /*
-    element.classList.remove("flip-container");
-    element.classList.remove("card");
-    element.classList.add("spacer")
-    */
+
+    //Поместить клон 1 в игровое пространство
+    newElement1.classList.add("avatar");
+    gameSpace.appendChild(newElement1);
+
+    //Поместить клон 2 в стэк
+    newElement2.className = "spacer stacked-card";
+    newElement2.innerHTML = null;
+    discardStack.appendChild(newElement2);
+
+    //Позиционировать клон 1 на месте исходной карты
+    var offSetTop = element.offsetTop;
+    var offSetLeft = element.offsetLeft;
+    var margTop = parseInt(getComputedStyle(element).marginTop, 10);
+    var margLeft = parseInt(getComputedStyle(element).marginLeft, 10);
+    var newTop = offSetTop - margTop;
+    var newLeft = offSetLeft - margLeft;
+    console.log(newTop + " : " + newLeft);
+    newElement1.style.top = newTop + "px";
+    newElement1.style.left = newLeft + "px";
+
+    //Открыть клон 1 карты
+    setTimeout(function () {
+        openCard(newElement1);
+    }, 0);
+
+    //Переместить клон 1 в стэк
+    setTimeout(function () {
+        newElement1.classList.add("small");
+        offSetTop = newElement2.offsetTop;
+        offSetLeft = newElement2.offsetLeft;
+        margTop = parseInt(getComputedStyle(newElement2).marginTop, 10);
+        margLeft = parseInt(getComputedStyle(newElement2).marginLeft, 10);
+        newTop = offSetTop - margTop;
+        newLeft = offSetLeft - margLeft;
+        //newTop = parseInt(newElement1.style.top, 10) + 100;
+        newElement1.style.top = newTop + "px";
+        newElement1.style.left = newLeft + "px";
+    }, 1000);
+
+
+    //Переместить карту в стэк
+    //discardCard(newElement);
 }
 
 function openCard(element) {
